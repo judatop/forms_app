50e4504739eb39aea2136c94317a095225556606
Juan García <judagarcia@outlook.com>
2023-07-17 01:22:44 -0500

    first commit

---
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_cubit/counter_cubit.dart';
+
+class CubitCounterScreen extends StatelessWidget {
+  const CubitCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (_) => CounterCubit(), child: const _CubirCounterView());
+  }
+}
+
+class _CubirCounterView extends StatelessWidget {
+  const _CubirCounterView();
+
+  void increaseCounterBy(BuildContext context, [int value = 1]) {
+    context.read<CounterCubit>().increaseBy(value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: context.select((CounterCubit value) {
+          return Text('Cubir Counter: ${value.state.transactionCount}');
+        }),
+        actions: [
+          IconButton(
+              onPressed: () {
+                context.read<CounterCubit>().reset();
+              },
+              icon: const Icon(Icons.refresh_outlined))
+        ],
+      ),
+      body: Center(child: BlocBuilder<CounterCubit, CounterState>(
+        builder: (context, state) {
+          return Text(
+            'Counter value: ${state.counter}',
+            style: const TextStyle(fontSize: 30),
+          );
+        },
+      )),
+      floatingActionButton:
+          Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+        FloatingActionButton(
+          heroTag: '1',
+          onPressed: () {
+            increaseCounterBy(context, 3);
+          },
+          child: const Text('+3'),
+        ),
+        const SizedBox(height: 15),
+        FloatingActionButton(
+          heroTag: '2',
+          child: const Text('+2'),
+          onPressed: () {
+            increaseCounterBy(context, 2);
+          },
+        ),
+        const SizedBox(height: 15),
+        FloatingActionButton(
+          heroTag: '3',
+          child: const Text('+1'),
+          onPressed: () {
+            increaseCounterBy(context, 1);
+          },
+        ),
+      ]),
+    );
+  }
+}
